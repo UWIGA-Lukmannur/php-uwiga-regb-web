3f66dc6662544ce091a47d457f0ed6effe5ba471
UWIGA-Lukmannur <luriz@ymail.com>
2014-01-26 10:48:18 +0700

    Update TestGitjava.java

--- src/testgitjava/TestGitjava.java
+++ src/testgitjava/TestGitjava.java
@@ -16,5 +16,6 @@ public class TestGitjava {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Tulisan Pertama");
+        System.out.println("Tulisan Kedua");
     }
 }
